2633cce2ffbf37295ab83d610c44d4c561f48463
GNUS <60074926+theMark001@users.noreply.github.com>
2025-06-18 18:57:53 +0200

    fix header

--- src/parser/parser.h
+++ src/parser/parser.h
@@ -1,3 +1,10 @@
+#ifndef PARSER_H
+# define PARSER_H
+
+#include "../init/init.h"
+
 t_list *parser(t_list *tokens);
 void print_groups(t_list *groups);
 void free_groups(t_list **groups);
+
+#endif
