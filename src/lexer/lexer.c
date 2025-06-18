f1839f4f0a0ffd1d19f36428b2d60c271e402a27
Denys Kot <kotdenys@fit.cvut.cz>
2025-06-18 17:11:29 +0200

    fix memleaks in lexer because I am cool guy

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -1,6 +1,5 @@
 #include "../header/header.h"
 
-
 int quotes_check(char *input)
 {
     int i = 0;
@@ -23,6 +22,26 @@ int quotes_check(char *input)
     return 1;
 }
 
+// Helper function to free a single token
+void free_token(t_token *token)
+{
+    if (token)
+    {
+        if (token->value)
+            free(token->value);
+        free(token);
+    }
+}
+
+// Helper function to clean up tokens list on error
+void cleanup_tokens(t_list **tokens)
+{
+    if (tokens && *tokens)
+    {
+        ft_free_tokens(tokens);
+    }
+}
+
 t_list *lexer(char *input)
 {
     if (!quotes_check(input))
@@ -36,20 +55,39 @@ t_list *lexer(char *input)
         char *start = ptr;
         t_token *token = malloc(sizeof(t_token));
         if (!token)
+        {
+            cleanup_tokens(&tokens);
             return NULL;
+        }
+
+        // Initialize token values to prevent issues
+        token->value = NULL;
+        token->value_len = 0;
 
         if (*ptr == ' ' || *ptr == '\t')
         {
             while (*ptr == ' ' || *ptr == '\t')
                 ptr++;
             token->type = SEP;
-            token->value = ft_strndup(ptr - 1, 1);
+            token->value = ft_strndup(start, 1);  // Fixed: use start instead of ptr-1
+            if (!token->value)
+            {
+                free(token);
+                cleanup_tokens(&tokens);
+                return NULL;
+            }
             token->value_len = 1;
         }
         else if (*ptr == '|')
         {
             token->type = PIPE;
             token->value = ft_strndup(ptr, 1);
+            if (!token->value)
+            {
+                free(token);
+                cleanup_tokens(&tokens);
+                return NULL;
+            }
             token->value_len = 1;
             ptr++;
         }
@@ -59,6 +97,12 @@ t_list *lexer(char *input)
             {
                 token->type = HEREDOC;
                 token->value = ft_strndup(ptr, 2);
+                if (!token->value)
+                {
+                    free(token);
+                    cleanup_tokens(&tokens);
+                    return NULL;
+                }
                 token->value_len = 2;
                 ptr += 2;
             }
@@ -66,6 +110,12 @@ t_list *lexer(char *input)
             {
                 token->type = REDIRECT_IN;
                 token->value = ft_strndup(ptr, 1);
+                if (!token->value)
+                {
+                    free(token);
+                    cleanup_tokens(&tokens);
+                    return NULL;
+                }
                 token->value_len = 1;
                 ptr++;
             }
@@ -76,6 +126,12 @@ t_list *lexer(char *input)
             {
                 token->type = REDIRECT_APPEND;
                 token->value = ft_strndup(ptr, 2);
+                if (!token->value)
+                {
+                    free(token);
+                    cleanup_tokens(&tokens);
+                    return NULL;
+                }
                 token->value_len = 2;
                 ptr += 2;
             }
@@ -83,6 +139,12 @@ t_list *lexer(char *input)
             {
                 token->type = REDIRECT_OUT;
                 token->value = ft_strndup(ptr, 1);
+                if (!token->value)
+                {
+                    free(token);
+                    cleanup_tokens(&tokens);
+                    return NULL;
+                }
                 token->value_len = 1;
                 ptr++;
             }
@@ -98,12 +160,19 @@ t_list *lexer(char *input)
                 size_t len = ptr - start;
                 token->type = FIELD;
                 token->value = ft_strndup(start, len);
+                if (!token->value)
+                {
+                    free(token);
+                    cleanup_tokens(&tokens);
+                    return NULL;
+                }
                 token->value_len = len;
                 ptr++;
             }
             else
             {
                 free(token);
+                cleanup_tokens(&tokens);
                 return NULL;
             }
         }
@@ -118,26 +187,48 @@ t_list *lexer(char *input)
                 size_t len = ptr - start;
                 token->type = EXP_FIELD;
                 token->value = ft_strndup(start, len);
+                if (!token->value)
+                {
+                    free(token);
+                    cleanup_tokens(&tokens);
+                    return NULL;
+                }
                 token->value_len = len;
                 ptr++;
             }
             else
             {
                 free(token);
+                cleanup_tokens(&tokens);
                 return NULL;
             }
         }
         else
         {
-            while (*ptr && *ptr != ' ' && *ptr != '\t' && *ptr != '|' && *ptr != '<' && *ptr != '>' && *ptr != '\'' && *ptr != '"')
+            while (*ptr && *ptr != ' ' && *ptr != '\t' && *ptr != '|' && 
+                   *ptr != '<' && *ptr != '>' && *ptr != '\'' && *ptr != '"')
                 ptr++;
             size_t len = ptr - start;
             token->type = WORD;
             token->value = ft_strndup(start, len);
+            if (!token->value)
+            {
+                free(token);
+                cleanup_tokens(&tokens);
+                return NULL;
+            }
             token->value_len = len;
         }
 
-        ft_lstadd_back(&tokens, ft_lstnew(token));
+        t_list *new_node = ft_lstnew(token);
+        if (!new_node)
+        {
+            free_token(token);
+            cleanup_tokens(&tokens);
+            return NULL;
+        }
+        ft_lstadd_back(&tokens, new_node);
+        
         if (*ptr == '\0')
             break;
     }
@@ -146,8 +237,12 @@ t_list *lexer(char *input)
 
 void process_input(char *line, t_info *info)
 {
+    if (!line)
+        return;
+        
     t_list *tokens = lexer(line);
-    if (tokens) {
+    if (tokens) 
+    {
         ft_print_tokens(tokens);
         ft_free_tokens(&tokens);
     }
