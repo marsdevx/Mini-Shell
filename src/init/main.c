1279e1d5b17496f4ea3626330f35c7f840c4029d
GNUS <60074926+theMark001@users.noreply.github.com>
2025-06-22 19:01:51 +0200

    CHECK POINT ADD NEW MAIN

--- src/init/main.c
+++ src/init/main.c
@@ -92,3 +92,65 @@ int main(int argc, char *argv[], char *envp[])
 
     return info.last_exit_status;
 }
+
+// static void ensure_env_defaults(void)
+// {
+//   if (!getenv("USER"))
+//   {
+//     const char *user = getenv("LOGNAME");
+//     setenv("USER", user ? user : "user", 1);
+//   }
+
+//   if (!getenv("HOME"))
+//   {
+//     setenv("HOME", "/home/user", 1);
+//   }
+// }
+
+// int main(int argc, char *argv[], char *envp[])
+// {
+//   t_info info;
+//   char *line = NULL;
+
+//   if (ft_init(&info) != 0)
+//     return EXIT_FAILURE;
+
+//   setenv("?", "0", 1);
+//   info.last_exit_status = 0;
+
+//   ensure_env_defaults();
+
+//   signal(SIGINT, handle_sigint);
+//   signal(SIGQUIT, SIG_IGN);
+
+//   if (argc > 2 && strcmp(argv[1], "-c") == 0)
+//   {
+//     line = strdup(argv[2]);
+//     if (!line)
+//       return EXIT_FAILURE;
+//     process_input(line, &info, envp);
+//     free(line);
+//   }
+//   else if (argc > 1)
+//   {
+//     fprintf(stderr, "minishell: %s: command not found\n", argv[1]);
+//     return 127;
+//   }
+//   else
+//   {
+//     while (info.exit_f)
+//     {
+//       line = ft_readline("minishell> ");
+//       if (!line)
+//       {
+//         printf("exit\n");
+//         break;
+//       }
+//       if (*line)
+//         process_input(line, &info, envp);
+//       free(line);
+//     }
+//   }
+
+//   return info.last_exit_status;
+// }
