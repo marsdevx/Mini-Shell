3a4f63f2d2e822abb63fad9baf89c6348c908ed0
GNUS <60074926+theMark001@users.noreply.github.com>
2025-06-24 21:14:09 +0200

    norm pipe.c

--- src/exec/pipe.c
+++ src/exec/pipe.c
@@ -6,231 +6,206 @@
 /*   By: marksylaiev <marksylaiev@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/23 13:14:05 by dkot              #+#    #+#             */
-/*   Updated: 2025/06/24 20:24:38 by marksylaiev      ###   ########.fr       */
+/*   Updated: 2025/06/24 21:13:31 by marksylaiev      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../init/header.h"
 
-
-int execute_pipeline(t_list *groups, t_exec_ctx *ctx)
+int	execute_pipeline(t_list *groups, t_exec_ctx *ctx)
 {
-    int pipe_count = 0;
-    t_list *tmp = groups;
-    
+	int pipe_count = 0;
+	t_list *tmp = groups;
 
-    while (tmp)
-    {
-        pipe_count++;
-        tmp = tmp->next;
-    }
-    
-    if (pipe_count == 1)
-    {
+	while (tmp)
+	{
+		pipe_count++;
+		tmp = tmp->next;
+	}
 
-        t_group *grp = (t_group *)groups->content;
-        return execute_single_command(grp, ctx);
-    }
-    
+	if (pipe_count == 1)
+	{
+		t_group *grp = (t_group *)groups->content;
+		return (execute_single_command(grp, ctx));
+	}
 
-    int (*pipes)[2] = malloc(sizeof(int[2]) * (pipe_count - 1));
-    if (!pipes)
-        return 1;
-    
+	int(*pipes)[2] = malloc(sizeof(int[2]) * (pipe_count - 1));
+	if (!pipes)
+		return (1);
 
-    for (int i = 0; i < pipe_count - 1; i++)
-    {
-        if (pipe(pipes[i]) < 0)
-        {
-            perror("pipe");
+	for (int i = 0; i < pipe_count - 1; i++)
+	{
+		if (pipe(pipes[i]) < 0)
+		{
+			perror("pipe");
 
-            for (int j = 0; j < i; j++)
-            {
-                close(pipes[j][0]);
-                close(pipes[j][1]);
-            }
-            free(pipes);
-            return 1;
-        }
-    }
-    
+			for (int j = 0; j < i; j++)
+			{
+				close(pipes[j][0]);
+				close(pipes[j][1]);
+			}
+			free(pipes);
+			return (1);
+		}
+	}
 
-    int cmd_index = 0;
-    pid_t *pids = malloc(sizeof(pid_t) * pipe_count);
-    if (!pids)
-    {
-        for (int i = 0; i < pipe_count - 1; i++)
-        {
-            close(pipes[i][0]);
-            close(pipes[i][1]);
-        }
-        free(pipes);
-        return 1;
-    }
-    
+	int cmd_index = 0;
+	pid_t *pids = malloc(sizeof(pid_t) * pipe_count);
+	if (!pids)
+	{
+		for (int i = 0; i < pipe_count - 1; i++)
+		{
+			close(pipes[i][0]);
+			close(pipes[i][1]);
+		}
+		free(pipes);
+		return (1);
+	}
 
-    while (groups)
-    {
-        t_group *grp = (t_group *)groups->content;
-        char **argv = group_to_argv(grp);
-        if (!argv)
-        {
+	while (groups)
+	{
+		t_group *grp = (t_group *)groups->content;
+		char **argv = group_to_argv(grp);
+		if (!argv)
+		{
+			for (int i = 0; i < cmd_index; i++)
+			{
+				kill(pids[i], SIGTERM);
+				waitpid(pids[i], NULL, 0);
+			}
+			for (int i = 0; i < pipe_count - 1; i++)
+			{
+				close(pipes[i][0]);
+				close(pipes[i][1]);
+			}
+			free(pids);
+			free(pipes);
+			return (1);
+		}
 
-            for (int i = 0; i < cmd_index; i++)
-            {
-                kill(pids[i], SIGTERM);
-                waitpid(pids[i], NULL, 0);
-            }
-            for (int i = 0; i < pipe_count - 1; i++)
-            {
-                close(pipes[i][0]);
-                close(pipes[i][1]);
-            }
-            free(pids);
-            free(pipes);
-            return 1;
-        }
-        
-        pids[cmd_index] = fork();
-        if (pids[cmd_index] < 0)
-        {
-            perror("fork");
-            free_argv(argv);
+		pids[cmd_index] = fork();
+		if (pids[cmd_index] < 0)
+		{
+			perror("fork");
+			free_argv(argv);
 
-            for (int i = 0; i < cmd_index; i++)
-            {
-                kill(pids[i], SIGTERM);
-                waitpid(pids[i], NULL, 0);
-            }
-            for (int i = 0; i < pipe_count - 1; i++)
-            {
-                close(pipes[i][0]);
-                close(pipes[i][1]);
-            }
-            free(pids);
-            free(pipes);
-            return 1;
-        }
-        else if (pids[cmd_index] == 0)
-        {
+			for (int i = 0; i < cmd_index; i++)
+			{
+				kill(pids[i], SIGTERM);
+				waitpid(pids[i], NULL, 0);
+			}
+			for (int i = 0; i < pipe_count - 1; i++)
+			{
+				close(pipes[i][0]);
+				close(pipes[i][1]);
+			}
+			free(pids);
+			free(pipes);
+			return (1);
+		}
+		else if (pids[cmd_index] == 0)
+		{
+			signal(SIGINT, SIG_DFL);
+			signal(SIGQUIT, SIG_DFL);
 
-            signal(SIGINT, SIG_DFL);
-            signal(SIGQUIT, SIG_DFL);
-            
+			if (cmd_index > 0)
+			{
+				dup2(pipes[cmd_index - 1][0], STDIN_FILENO);
+			}
 
-            if (cmd_index > 0)
-            {
+			if (cmd_index < pipe_count - 1)
+			{
+				dup2(pipes[cmd_index][1], STDOUT_FILENO);
+			}
 
-                dup2(pipes[cmd_index - 1][0], STDIN_FILENO);
-            }
-            
-            if (cmd_index < pipe_count - 1)
-            {
+			for (int i = 0; i < pipe_count - 1; i++)
+			{
+				close(pipes[i][0]);
+				close(pipes[i][1]);
+			}
 
-                dup2(pipes[cmd_index][1], STDOUT_FILENO);
-            }
-            
+			int redir_status = setup_redirections(&argv);
 
-            for (int i = 0; i < pipe_count - 1; i++)
-            {
-                close(pipes[i][0]);
-                close(pipes[i][1]);
-            }
-            
+			if (!argv[0] || strlen(argv[0]) == 0)
+			{
+				exit(0);
+			}
 
-            int redir_status = setup_redirections(&argv);
-            
+			if (redir_status < 0)
+				exit(1);
 
-            if (!argv[0] || strlen(argv[0]) == 0)
-            {
+			if (is_builtin(argv[0]))
+			{
+				t_exec_ctx child_ctx = *ctx;
 
-                exit(0);
-            }
-            
+				t_info child_info = *ctx->info;
+				child_ctx.info = &child_info;
 
-            if (redir_status < 0)
-                exit(1);
-                
-            if (is_builtin(argv[0]))
-            {
+				int status = execute_builtin(argv, &child_ctx);
+				exit(status);
+			}
+			else
+			{
+				char *cmd_path = resolve_command_path(argv[0]);
+				if (!cmd_path)
+				{
+					fprintf(stderr, "bash: %s: command not found\n", argv[0]);
+					exit(127);
+				}
 
-                t_exec_ctx child_ctx = *ctx;
+				struct stat st;
+				if (stat(cmd_path, &st) == 0 && S_ISDIR(st.st_mode))
+				{
+					fprintf(stderr, "bash: %s: Is a directory\n", argv[0]);
+					free(cmd_path);
+					exit(126);
+				}
 
-                t_info child_info = *ctx->info;
-                child_ctx.info = &child_info;
-                
-                int status = execute_builtin(argv, &child_ctx);
-                exit(status);
-            }
-            else
-            {
+				if (access(cmd_path, X_OK) != 0 && access(cmd_path, F_OK) == 0)
+				{
+					fprintf(stderr, "bash: %s: Permission denied\n", argv[0]);
+					free(cmd_path);
+					exit(126);
+				}
 
-                char *cmd_path = resolve_command_path(argv[0]);
-                if (!cmd_path)
-                {
-                    fprintf(stderr, "bash: %s: command not found\n", argv[0]);
-                    exit(127);
-                }
-                
+				execve(cmd_path, argv, ctx->envp);
+				perror(argv[0]);
+				exit(126);
+			}
+		}
 
-                struct stat st;
-                if (stat(cmd_path, &st) == 0 && S_ISDIR(st.st_mode))
-                {
-                    fprintf(stderr, "bash: %s: Is a directory\n", argv[0]);
-                    free(cmd_path);
-                    exit(126);
-                }
-                
+		free_argv(argv);
+		groups = groups->next;
+		cmd_index++;
+	}
 
-                if (access(cmd_path, X_OK) != 0 && access(cmd_path, F_OK) == 0)
-                {
-                    fprintf(stderr, "bash: %s: Permission denied\n", argv[0]);
-                    free(cmd_path);
-                    exit(126);
-                }
-                
-                execve(cmd_path, argv, ctx->envp);
-                perror(argv[0]);
-                exit(126);
-            }
-        }
-        
+	for (int i = 0; i < pipe_count - 1; i++)
+	{
+		close(pipes[i][0]);
+		close(pipes[i][1]);
+	}
 
-        free_argv(argv);
-        groups = groups->next;
-        cmd_index++;
-    }
-    
+	int status = 0;
+	for (int i = 0; i < pipe_count; i++)
+	{
+		int child_status;
+		if (waitpid(pids[i], &child_status, 0) < 0)
+		{
+			perror("waitpid");
+		}
+		else
+		{
+			if (i == pipe_count - 1)
+			{
+				if (WIFEXITED(child_status))
+					status = WEXITSTATUS(child_status);
+				else if (WIFSIGNALED(child_status))
+					status = 128 + WTERMSIG(child_status);
+			}
+		}
+	}
 
-    for (int i = 0; i < pipe_count - 1; i++)
-    {
-        close(pipes[i][0]);
-        close(pipes[i][1]);
-    }
-    
-
-    int status = 0;
-    for (int i = 0; i < pipe_count; i++)
-    {
-        int child_status;
-        if (waitpid(pids[i], &child_status, 0) < 0)
-        {
-            perror("waitpid");
-        }
-        else
-        {
-
-            if (i == pipe_count - 1)
-            {
-                if (WIFEXITED(child_status))
-                    status = WEXITSTATUS(child_status);
-                else if (WIFSIGNALED(child_status))
-                    status = 128 + WTERMSIG(child_status);
-            }
-        }
-    }
-    
-    free(pids);
-    free(pipes);
-    return status;
+	free(pids);
+	free(pipes);
+	return (status);
 }
