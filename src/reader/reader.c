2c75637f73a27613856e533f41288b1a502cded0
GNUS <60074926+theMark001@users.noreply.github.com>
2025-06-24 19:26:25 +0200

    norm reader.c

--- src/reader/reader.c
+++ src/reader/reader.c
@@ -1,27 +1,31 @@
 #include "../init/header.h"
 
-char *ft_readline(const char *prompt)
+char	*ft_readline(const char *prompt)
 {
-    char *line = readline(prompt);
-    if (line && *line) {
-        add_history(line);
-    }
-    return line;
+	char	*line;
+
+	line = readline(prompt);
+	if (line && *line)
+	{
+		add_history(line);
+	}
+	return (line);
 }
 
-int ft_init(t_info *info)
+int	ft_init(t_info *info)
 {
-    info->exit_f = 1;
-    info->last_exit_status = 0;
-    return 0;
+	info->exit_f = 1;
+	info->last_exit_status = 0;
+	return (0);
 }
 
-void handle_sigint(int sig)
+void	handle_sigint(int sig)
 {
-    if (sig == SIGINT) {
-        printf("\n");
-        rl_on_new_line();
-        rl_replace_line("", 0);
-        rl_redisplay();
-    }
+	if (sig == SIGINT)
+	{
+		printf("\n");
+		rl_on_new_line();
+		rl_replace_line("", 0);
+		rl_redisplay();
+	}
 }
