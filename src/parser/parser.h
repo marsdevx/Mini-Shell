f2b1c4788e6db254f3a420edd785d6baebd12790
GNUS <60074926+theMark001@users.noreply.github.com>
2025-06-24 20:12:14 +0200

    norm parser.h

--- src/parser/parser.h
+++ src/parser/parser.h
@@ -13,9 +13,9 @@
 #ifndef PARSER_H
 # define PARSER_H
 
-#include "../init/init.h"
+# include "../init/init.h"
 
-t_list *parser(t_list *tokens);
-void free_groups(t_list **groups);
+t_list	*parser(t_list *tokens);
+void	free_groups(t_list **groups);
 
 #endif
