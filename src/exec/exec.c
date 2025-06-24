9066687dca4c789a07b08902f80780834f521141
GNUS <60074926+theMark001@users.noreply.github.com>
2025-06-24 20:51:15 +0200

    norm exec.c

--- src/exec/exec.c
+++ src/exec/exec.c
@@ -12,149 +12,147 @@
 
 #include "../init/header.h"
 
-char **group_to_argv(t_group *grp)
+char	**group_to_argv(t_group *grp)
 {
-    int count = count_args(grp->argv);
-    char **argv = malloc(sizeof(char *) * (count + 1));
-    if (!argv)
-        return NULL;
-    
-    int i = 0;
-    t_list *current = grp->argv;
-    while (current)
-    {
-        t_command *cmd = (t_command *)current->content;
-        argv[i] = strdup(cmd->arg);
-        if (!argv[i])
-        {
-            while (--i >= 0)
-                free(argv[i]);
-            free(argv);
-            return NULL;
-        }
-        i++;
-        current = current->next;
-    }
-    argv[i] = NULL;
-    return argv;
+	int			count;
+	char		**argv;
+	int			i;
+	t_list		*current;
+	t_command	*cmd;
+
+	count = count_args(grp->argv);
+	argv = malloc(sizeof(char *) * (count + 1));
+	if (!argv)
+		return (NULL);
+	i = 0;
+	current = grp->argv;
+	while (current)
+	{
+		cmd = (t_command *)current->content;
+		argv[i] = strdup(cmd->arg);
+		if (!argv[i])
+		{
+			while (--i >= 0)
+				free(argv[i]);
+			free(argv);
+			return (NULL);
+		}
+		i++;
+		current = current->next;
+	}
+	argv[i] = NULL;
+	return (argv);
 }
 
-int count_args(t_list *args)
+int	count_args(t_list *args)
 {
-    int count = 0;
-    while (args)
-    {
-        count++;
-        args = args->next;
-    }
-    return count;
+	int	count;
+
+	count = 0;
+	while (args)
+	{
+		count++;
+		args = args->next;
+	}
+	return (count);
 }
 
-void free_argv(char **argv)
+void	free_argv(char **argv)
 {
-    if (!argv)
-        return;
-    for (int i = 0; argv[i]; i++)
-        free(argv[i]);
-    free(argv);
+	if (!argv)
+		return ;
+	for (int i = 0; argv[i]; i++)
+		free(argv[i]);
+	free(argv);
 }
 
-int execute_commands(t_list *groups, char **envp, t_info *info)
+int	execute_commands(t_list *groups, char **envp, t_info *info)
 {
-    t_exec_ctx ctx;
-    
-    ctx.stdin_backup = dup(STDIN_FILENO);
-    ctx.stdout_backup = dup(STDOUT_FILENO);
-    ctx.last_exit_status = info->last_exit_status;
-    ctx.envp = envp;
-    ctx.info = info;
-    
-    if (!groups)
-    {
-        close(ctx.stdin_backup);
-        close(ctx.stdout_backup);
-        return info->last_exit_status;
-    }
-    
-    int group_count = 0;
-    t_list *tmp = groups;
-    while (tmp)
-    {
-        group_count++;
-        tmp = tmp->next;
-    }
-    
-    if (group_count > 1)
-    {
-        ctx.last_exit_status = execute_pipeline(groups, &ctx);
-    }
-    else if (groups)
-    {
-        t_group *grp = (t_group *)groups->content;
-        ctx.last_exit_status = execute_single_command(grp, &ctx);
-    }
-    
-    close(ctx.stdin_backup);
-    close(ctx.stdout_backup);
-    
-    info->last_exit_status = ctx.last_exit_status;
-    
-    char exit_str[16];
-    snprintf(exit_str, sizeof(exit_str), "%d", ctx.last_exit_status);
-    setenv("?", exit_str, 1);
-    
-    return ctx.last_exit_status;
+	t_exec_ctx	ctx;
+	int			group_count;
+	t_list		*tmp;
+	t_group		*grp;
+	char		exit_str[16];
+
+	ctx.stdin_backup = dup(STDIN_FILENO);
+	ctx.stdout_backup = dup(STDOUT_FILENO);
+	ctx.last_exit_status = info->last_exit_status;
+	ctx.envp = envp;
+	ctx.info = info;
+	if (!groups)
+	{
+		close(ctx.stdin_backup);
+		close(ctx.stdout_backup);
+		return (info->last_exit_status);
+	}
+	group_count = 0;
+	tmp = groups;
+	while (tmp)
+	{
+		group_count++;
+		tmp = tmp->next;
+	}
+	if (group_count > 1)
+	{
+		ctx.last_exit_status = execute_pipeline(groups, &ctx);
+	}
+	else if (groups)
+	{
+		grp = (t_group *)groups->content;
+		ctx.last_exit_status = execute_single_command(grp, &ctx);
+	}
+	close(ctx.stdin_backup);
+	close(ctx.stdout_backup);
+	info->last_exit_status = ctx.last_exit_status;
+	snprintf(exit_str, sizeof(exit_str), "%d", ctx.last_exit_status);
+	setenv("?", exit_str, 1);
+	return (ctx.last_exit_status);
 }
 
-int execute_single_command(t_group *grp, t_exec_ctx *ctx)
+int	execute_single_command(t_group *grp, t_exec_ctx *ctx)
 {
-    if (!grp || !grp->argv)
-        return 0;
-    
-    char **argv = group_to_argv(grp);
-    if (!argv)
-        return 1;
-    
-    if (!argv[0])
-    {
-        free_argv(argv);
-        return 0;
-    }
-    
-    int status = 0;
-    
-    int stdin_temp = dup(STDIN_FILENO);
-    int stdout_temp = dup(STDOUT_FILENO);
-    
-    if (setup_redirections(&argv) < 0)
-    {
-        status = 1;
-        goto cleanup;
-    }
-    
-    if (!argv[0])
-    {
-        status = 0;
-        goto cleanup;
-    }
-    
-    if (strlen(argv[0]) == 0)
-    {
-        status = 0;
-        goto cleanup;
-    }
-    
-    if (is_builtin(argv[0]))
-        status = execute_builtin(argv, ctx);
-    else
-        status = execute_external(argv, ctx);
-    
+	char	**argv;
+	int		status;
+	int		stdin_temp;
+	int		stdout_temp;
+
+	if (!grp || !grp->argv)
+		return (0);
+	argv = group_to_argv(grp);
+	if (!argv)
+		return (1);
+	if (!argv[0])
+	{
+		free_argv(argv);
+		return (0);
+	}
+	status = 0;
+	stdin_temp = dup(STDIN_FILENO);
+	stdout_temp = dup(STDOUT_FILENO);
+	if (setup_redirections(&argv) < 0)
+	{
+		status = 1;
+		goto cleanup;
+	}
+	if (!argv[0])
+	{
+		status = 0;
+		goto cleanup;
+	}
+	if (strlen(argv[0]) == 0)
+	{
+		status = 0;
+		goto cleanup;
+	}
+	if (is_builtin(argv[0]))
+		status = execute_builtin(argv, ctx);
+	else
+		status = execute_external(argv, ctx);
 cleanup:
-    dup2(stdin_temp, STDIN_FILENO);
-    dup2(stdout_temp, STDOUT_FILENO);
-    close(stdin_temp);
-    close(stdout_temp);
-    
-    free_argv(argv);
-    return status;
+	dup2(stdin_temp, STDIN_FILENO);
+	dup2(stdout_temp, STDOUT_FILENO);
+	close(stdin_temp);
+	close(stdout_temp);
+	free_argv(argv);
+	return (status);
 }
