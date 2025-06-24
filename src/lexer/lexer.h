6cd327de6f67c756cc4af425c62aa054318a3cca
GNUS <60074926+theMark001@users.noreply.github.com>
2025-06-24 19:29:50 +0200

    norm lexer.h

--- src/lexer/lexer.h
+++ src/lexer/lexer.h
@@ -1,10 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dkot <dkot@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/23 13:14:10 by dkot              #+#    #+#             */
+/*   Updated: 2025/06/23 13:17:42 by dkot             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef LEXER_H
 # define LEXER_H
 
-#include "../init/init.h"
+# include "../init/init.h"
 
-int quotes_check(char *input);
-t_list *lexer(char *input);
-
+int		quotes_check(char *input);
+t_list	*lexer(char *input);
 
 #endif
