e8db71596d52f15fe3d6725bfc14089ab5e81503
GNUS <60074926+theMark001@users.noreply.github.com>
2025-06-24 20:19:59 +0200

    norm external.c

--- src/exec/external.c
+++ src/exec/external.c
@@ -6,75 +6,66 @@
 /*   By: marksylaiev <marksylaiev@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/23 13:14:05 by dkot              #+#    #+#             */
-/*   Updated: 2025/06/24 20:00:18 by marksylaiev      ###   ########.fr       */
+/*   Updated: 2025/06/24 20:19:29 by marksylaiev      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../init/header.h"
 
-int execute_external(char **args, t_exec_ctx *ctx)
+int	execute_external(char **args, t_exec_ctx *ctx)
 {
-    char *cmd_path = resolve_command_path(args[0]);
-    if (!cmd_path)
-    {
-        fprintf(stderr, "bash: %s: command not found\n", args[0]);
-        return 127;
-    }
-    
-    struct stat st;
-    if (stat(cmd_path, &st) == 0 && S_ISDIR(st.st_mode))
-    {
-        fprintf(stderr, "bash: %s: Is a directory\n", args[0]);
-        free(cmd_path);
-        return 126;
-    }
-    
-    if (access(cmd_path, X_OK) != 0 && access(cmd_path, F_OK) == 0)
-    {
-        fprintf(stderr, "bash: %s: Permission denied\n", args[0]);
-        free(cmd_path);
-        return 126;
-    }
-    
-    pid_t pid = fork();
-    if (pid < 0)
-    {
-        perror("fork");
-        free(cmd_path);
-        return 1;
-    }
-    else if (pid == 0)
-    {
-        signal(SIGINT, SIG_DFL);
-        signal(SIGQUIT, SIG_DFL);
-        
-        execve(cmd_path, args, ctx->envp);
-        
+	char		*cmd_path;
+	struct stat	st;
+	pid_t		pid;
+	int			status;
 
-        perror(args[0]);
-        exit(126);
-    }
-    else
-    {
-
-        int status;
-        
-
-        if (waitpid(pid, &status, 0) < 0)
-        {
-            perror("waitpid");
-            free(cmd_path);
-            return 1;
-        }
-        
-        free(cmd_path);
-        
-
-        if (WIFEXITED(status))
-            return WEXITSTATUS(status);
-        else if (WIFSIGNALED(status))
-            return 128 + WTERMSIG(status);
-        else
-            return 1;
-    }
+	cmd_path = resolve_command_path(args[0]);
+	if (!cmd_path)
+	{
+		fprintf(stderr, "bash: %s: command not found\n", args[0]);
+		return (127);
+	}
+	if (stat(cmd_path, &st) == 0 && S_ISDIR(st.st_mode))
+	{
+		fprintf(stderr, "bash: %s: Is a directory\n", args[0]);
+		free(cmd_path);
+		return (126);
+	}
+	if (access(cmd_path, X_OK) != 0 && access(cmd_path, F_OK) == 0)
+	{
+		fprintf(stderr, "bash: %s: Permission denied\n", args[0]);
+		free(cmd_path);
+		return (126);
+	}
+	pid = fork();
+	if (pid < 0)
+	{
+		perror("fork");
+		free(cmd_path);
+		return (1);
+	}
+	else if (pid == 0)
+	{
+		signal(SIGINT, SIG_DFL);
+		signal(SIGQUIT, SIG_DFL);
+		execve(cmd_path, args, ctx->envp);
+		perror(args[0]);
+		exit(126);
+	}
+	else
+	{
+		if (waitpid(pid, &status, 0) < 0)
+		{
+			perror("waitpid");
+			free(cmd_path);
+			return (1);
+		}
+		free(cmd_path);
+		if (WIFEXITED(status))
+			return (WEXITSTATUS(status));
+		else if (WIFSIGNALED(status))
+			return (128 + WTERMSIG(status));
+		else
+			return (1);
+	}
 }
