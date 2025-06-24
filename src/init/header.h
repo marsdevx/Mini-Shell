c9ccf50559b2e70a583609d9c19d17a8eef5805d
GNUS <60074926+theMark001@users.noreply.github.com>
2025-06-24 20:40:40 +0200

    norm header.h

--- src/init/header.h
+++ src/init/header.h
@@ -13,25 +13,25 @@
 #ifndef HEADER_H
 # define HEADER_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-#include <readline/readline.h>
-#include <readline/history.h>
-#include <signal.h>
-#include <sys/wait.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <errno.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <ctype.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <signal.h>
+# include <sys/wait.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <errno.h>
 
-#include "../utils/utils.h"
-#include "../lexer/lexer.h"
-#include "../reader/reader.h"
-#include "../parser/parser.h"
-#include "../builtin/builtin.h"
-#include "../exec/exec.h"
+# include "../builtin/builtin.h"
+# include "../exec/exec.h"
+# include "../lexer/lexer.h"
+# include "../parser/parser.h"
+# include "../reader/reader.h"
+# include "../utils/utils.h"
 
 #endif
