8253b564481aa52213ff64fd640515c661d4f29d
Denys Kot <kotdenys@fit.cvut.cz>
2025-06-26 18:46:24 +0200

    strtol fix

--- src/exec/path.c
+++ src/exec/path.c
@@ -6,7 +6,7 @@
 /*   By: dkot <dkot@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/23 13:14:05 by dkot              #+#    #+#             */
-/*   Updated: 2025/06/26 18:29:07 by dkot             ###   ########.fr       */
+/*   Updated: 2025/06/26 18:45:22 by dkot             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,8 @@ static char	*try_path(const char *dir, const char *cmd)
 	size_t	cmd_len;
 	char	*full_path;
 
-	dir_len = strlen(dir);
-	cmd_len = strlen(cmd);
+	dir_len = ft_strlen(dir);
+	cmd_len = ft_strlen(cmd);
 	full_path = malloc(dir_len + cmd_len + 2);
 	if (!full_path)
 		return (NULL);
@@ -38,31 +38,57 @@ static char	*try_path(const char *dir, const char *cmd)
 	return (NULL);
 }
 
+static char	*get_next_path_dir(char **path_ptr)
+{
+	char	*start;
+	char	*end;
+	char	*dir;
+	size_t	len;
+
+	if (!path_ptr || !*path_ptr || **path_ptr == '\0')
+		return (NULL);
+	start = *path_ptr;
+	end = ft_strchr(start, ':');
+	if (end)
+	{
+		len = end - start;
+		*path_ptr = end + 1;
+	}
+	else
+	{
+		len = ft_strlen(start);
+		*path_ptr = start + len;
+	}
+	dir = malloc(len + 1);
+	if (!dir)
+		return (NULL);
+	ft_strncpy(dir, start, len);
+	dir[len] = '\0';
+	return (dir);
+}
+
 char	*resolve_command_path(const char *cmd)
 {
 	char	*path_env;
 	char	*path_copy;
+	char	*path_ptr;
 	char	*dir;
 	char	*result;
 
 	if (is_path_command(cmd))
-	{
 		return (ft_strdup(cmd));
-	}
 	path_env = getenv("PATH");
 	if (!path_env)
-	{
 		return (NULL);
-	}
 	path_copy = ft_strdup(path_env);
 	if (!path_copy)
 		return (NULL);
-	dir = strtok(path_copy, ":");
+	path_ptr = path_copy;
 	result = NULL;
-	while (dir && !result)
+	while (!result && (dir = get_next_path_dir(&path_ptr)) != NULL)
 	{
 		result = try_path(dir, cmd);
-		dir = strtok(NULL, ":");
+		free(dir);
 	}
 	free(path_copy);
 	return (result);
