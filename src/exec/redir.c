ff80709303d587be3ea501ad7ae65f7451230561
GNUS <60074926+theMark001@users.noreply.github.com>
2025-07-08 22:47:57 +0200

    split redirections.c

--- src/exec/redir.c
+++ src/exec/redir.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   redirections.c                                     :+:      :+:    :+:   */
+/*   redir.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: marksylaiev <marksylaiev@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/23 13:14:05 by dkot              #+#    #+#             */
-/*   Updated: 2025/07/08 22:43:14 by marksylaiev      ###   ########.fr       */
+/*   Updated: 2025/07/08 22:46:39 by marksylaiev      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -89,72 +89,6 @@ int	setup_redirections(char ***argv_ptr)
 			i++;
 	}
 	return (last_error);
-}
-
-int	handle_input_redirect(const char *filename)
-{
-	int	fd;
-
-	fd = open(filename, O_RDONLY);
-	if (fd < 0)
-	{
-		write_error_with_arg("bash: ", filename, ": ");
-		write_error(strerror(errno));
-		write_error("\n");
-		return (-1);
-	}
-	if (dup2(fd, STDIN_FILENO) < 0)
-	{
-		perror("dup2");
-		close(fd);
-		return (-1);
-	}
-	close(fd);
-	return (0);
-}
-
-int	handle_output_redirect(const char *filename)
-{
-	int	fd;
-
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (fd < 0)
-	{
-		write_error_with_arg("bash: ", filename, ": ");
-		write_error(strerror(errno));
-		write_error("\n");
-		return (-1);
-	}
-	if (dup2(fd, STDOUT_FILENO) < 0)
-	{
-		perror("dup2");
-		close(fd);
-		return (-1);
-	}
-	close(fd);
-	return (0);
-}
-
-int	handle_append_redirect(const char *filename)
-{
-	int	fd;
-
-	fd = open(filename, O_WRONLY | O_CREAT | O_APPEND, 0644);
-	if (fd < 0)
-	{
-		write_error_with_arg("bash: ", filename, ": ");
-		write_error(strerror(errno));
-		write_error("\n");
-		return (-1);
-	}
-	if (dup2(fd, STDOUT_FILENO) < 0)
-	{
-		perror("dup2");
-		close(fd);
-		return (-1);
-	}
-	close(fd);
-	return (0);
 }
 
 int	handle_heredoc(const char *delimiter)
---
+++ src/exec/redir_utils.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   redir_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marksylaiev <marksylaiev@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/23 13:14:05 by dkot              #+#    #+#             */
+/*   Updated: 2025/07/08 22:46:36 by marksylaiev      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../init/header.h"
+
+int	handle_input_redirect(const char *filename)
+{
+	int	fd;
+
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+	{
+		write_error_with_arg("bash: ", filename, ": ");
+		write_error(strerror(errno));
+		write_error("\n");
+		return (-1);
+	}
+	if (dup2(fd, STDIN_FILENO) < 0)
+	{
+		perror("dup2");
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (0);
+}
+
+int	handle_output_redirect(const char *filename)
+{
+	int	fd;
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (fd < 0)
+	{
+		write_error_with_arg("bash: ", filename, ": ");
+		write_error(strerror(errno));
+		write_error("\n");
+		return (-1);
+	}
+	if (dup2(fd, STDOUT_FILENO) < 0)
+	{
+		perror("dup2");
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (0);
+}
+
+int	handle_append_redirect(const char *filename)
+{
+	int	fd;
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_APPEND, 0644);
+	if (fd < 0)
+	{
+		write_error_with_arg("bash: ", filename, ": ");
+		write_error(strerror(errno));
+		write_error("\n");
+		return (-1);
+	}
+	if (dup2(fd, STDOUT_FILENO) < 0)
+	{
+		perror("dup2");
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (0);
+}
