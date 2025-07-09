c8d33f2020b560731fbf9f39dff5398c86fb1176
Denys Kot <kotdenys@fit.cvut.cz>
2025-07-09 19:50:39 +0200

    external

--- src/exec/external.c
+++ src/exec/external.c
@@ -3,21 +3,81 @@
 /*                                                        :::      ::::::::   */
 /*   external.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marksylaiev <marksylaiev@student.42.fr>    +#+  +:+       +#+        */
+/*   By: dkot <dkot@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/23 13:14:05 by dkot              #+#    #+#             */
-/*   Updated: 2025/07/08 22:33:50 by marksylaiev      ###   ########.fr       */
+/*   Updated: 2025/07/09 19:43:25 by dkot             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../init/header.h"
 
+static int	validate_command(char *cmd_path, char *cmd_name)
+{
+	struct stat	st;
+
+	if (stat(cmd_path, &st) == 0 && S_ISDIR(st.st_mode))
+	{
+		write_error_with_arg("bash: ", cmd_name, ": Is a directory\n");
+		return (126);
+	}
+	if (access(cmd_path, X_OK) != 0 && access(cmd_path, F_OK) == 0)
+	{
+		write_error_with_arg("bash: ", cmd_name, ": Permission denied\n");
+		return (126);
+	}
+	return (0);
+}
+
+static void	child_process(char *cmd_path, char **args, char **env)
+{
+	signal(SIGINT, SIG_DFL);
+	signal(SIGQUIT, SIG_DFL);
+	execve(cmd_path, args, env);
+	perror(args[0]);
+	exit(127);
+}
+
+static int	get_exit_status(int status)
+{
+	if (WIFEXITED(status))
+		return (WEXITSTATUS(status));
+	else if (WIFSIGNALED(status))
+		return (128 + WTERMSIG(status));
+	else
+		return (1);
+}
+
+static int	fork_and_execute(char *cmd_path, char **args, char **env)
+{
+	pid_t	pid;
+	int		status;
+
+	pid = fork();
+	if (pid < 0)
+	{
+		perror("fork");
+		return (1);
+	}
+	else if (pid == 0)
+		child_process(cmd_path, args, env);
+	else
+	{
+		if (waitpid(pid, &status, 0) < 0)
+		{
+			perror("waitpid");
+			return (1);
+		}
+		return (get_exit_status(status));
+	}
+	return (1);
+}
+
 int	execute_external(char **args, t_exec_ctx *ctx)
 {
-	char		*cmd_path;
-	struct stat	st;
-	pid_t		pid;
-	int			status;
+	char	*cmd_path;
+	int		validation_result;
+	int		exit_status;
 
 	cmd_path = resolve_command_path(args[0], ctx->info->env);
 	if (!cmd_path)
@@ -25,47 +85,13 @@ int	execute_external(char **args, t_exec_ctx *ctx)
 		write_error_with_arg("bash: ", args[0], ": command not found\n");
 		return (127);
 	}
-	if (stat(cmd_path, &st) == 0 && S_ISDIR(st.st_mode))
+	validation_result = validate_command(cmd_path, args[0]);
+	if (validation_result != 0)
 	{
-		write_error_with_arg("bash: ", args[0], ": Is a directory\n");
 		free(cmd_path);
-		return (126);
+		return (validation_result);
 	}
-	if (access(cmd_path, X_OK) != 0 && access(cmd_path, F_OK) == 0)
-	{
-		write_error_with_arg("bash: ", args[0], ": Permission denied\n");
-		free(cmd_path);
-		return (126);
-	}
-	pid = fork();
-	if (pid < 0)
-	{
-		perror("fork");
-		free(cmd_path);
-		return (1);
-	}
-	else if (pid == 0)
-	{
-		signal(SIGINT, SIG_DFL);
-		signal(SIGQUIT, SIG_DFL);
-		execve(cmd_path, args, ctx->info->env);
-		perror(args[0]);
-		exit(127);
-	}
-	else
-	{
-		if (waitpid(pid, &status, 0) < 0)
-		{
-			perror("waitpid");
-			free(cmd_path);
-			return (1);
-		}
-		free(cmd_path);
-		if (WIFEXITED(status))
-			return (WEXITSTATUS(status));
-		else if (WIFSIGNALED(status))
-			return (128 + WTERMSIG(status));
-		else
-			return (1);
-	}
+	exit_status = fork_and_execute(cmd_path, args, ctx->info->env);
+	free(cmd_path);
+	return (exit_status);
 }
