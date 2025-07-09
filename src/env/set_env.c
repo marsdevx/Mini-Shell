73b40aaea7521198aa7f1c2f8615602bbaf03c8f
GNUS <60074926+theMark001@users.noreply.github.com>
2025-07-09 21:28:20 +0200

    env

--- src/env/set_env.c
+++ src/env/set_env.c
@@ -6,7 +6,7 @@
 /*   By: marksylaiev <marksylaiev@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/26 13:14:05 by dkot              #+#    #+#             */
-/*   Updated: 2025/07/08 22:32:34 by marksylaiev      ###   ########.fr       */
+/*   Updated: 2025/07/09 21:27:04 by marksylaiev      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,46 +51,66 @@ static int	count_env_vars(char **env)
 	return (count);
 }
 
-char	**set_env_var(char **env, const char *name, const char *value,
-		int overwrite)
+static char	*build_env_entry(const char *name, const char *value)
 {
-	char	*new_var;
+	char	*entry;
+
+	entry = malloc(ft_strlen(name) + ft_strlen(value) + 2);
+	if (!entry)
+		return (NULL);
+	ft_strcpy(entry, name);
+	ft_strcat(entry, "=");
+	ft_strcat(entry, value);
+	return (entry);
+}
+
+static char	**update_env(char **env, char *new_entry, int index, int overwrite)
+{
 	char	**new_env;
-	int		index;
 	int		count;
 	int		i;
 
-	if (!name || !value || ft_strchr(name, '='))
-		return (env);
-	new_var = malloc(ft_strlen(name) + ft_strlen(value) + 2);
-	if (!new_var)
-		return (env);
-	ft_strcpy(new_var, name);
-	ft_strcat(new_var, "=");
-	ft_strcat(new_var, value);
-	index = find_env_var(env, name);
 	if (index >= 0)
 	{
 		if (!overwrite)
 		{
-			free(new_var);
+			free(new_entry);
 			return (env);
 		}
 		free(env[index]);
-		env[index] = new_var;
+		env[index] = new_entry;
 		return (env);
 	}
 	count = count_env_vars(env);
 	new_env = malloc(sizeof(char *) * (count + 2));
 	if (!new_env)
-	{
-		free(new_var);
-		return (env);
-	}
-	for (i = 0; i < count; i++)
+		return (NULL);
+	for (i = 0; i < count; ++i)
 		new_env[i] = env[i];
-	new_env[count] = new_var;
+	new_env[count] = new_entry;
 	new_env[count + 1] = NULL;
 	free(env);
 	return (new_env);
+}
+
+char	**set_env_var(char **env, const char *name, const char *value,
+		int overwrite)
+{
+	char	*new_entry;
+	char	**result;
+	int		index;
+
+	if (!name || !value || ft_strchr(name, '='))
+		return (env);
+	new_entry = build_env_entry(name, value);
+	if (!new_entry)
+		return (env);
+	index = find_env_var(env, name);
+	result = update_env(env, new_entry, index, overwrite);
+	if (!result)
+	{
+		free(new_entry);
+		return (env);
+	}
+	return (result);
 }
