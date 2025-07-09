ecbb15da5a32da65cf68654bdb882a3c3c1bef29
GNUS <60074926+theMark001@users.noreply.github.com>
2025-07-09 11:34:28 +0200

    builtin/utils.c

--- src/builtin/utils.c
+++ src/builtin/utils.c
@@ -59,7 +59,7 @@ int	builtin_pwd(char **args, t_exec_ctx *ctx)
 
 int	builtin_env(char **args, t_exec_ctx *ctx)
 {
-	int i;
+	int	i;
 
 	(void)args;
 	i = 0;
@@ -71,43 +71,43 @@ int	builtin_env(char **args, t_exec_ctx *ctx)
 	return (0);
 }
 
-int builtin_unset(char **args, t_exec_ctx *ctx)
+int	builtin_unset(char **args, t_exec_ctx *ctx)
 {
-    if (!args[1])
-        return (0);
-    
-    for (int i = 1; args[i]; i++)
-    {
-        if (!is_valid_identifier(args[i]))
-            continue;
-            
-        ctx->info->env = unset_env_var(ctx->info->env, args[i]);
-    }
-    
-    return (0);
+	if (!args[1])
+		return (0);
+	for (int i = 1; args[i]; i++)
+	{
+		if (!is_valid_identifier(args[i]))
+			continue ;
+		ctx->info->env = unset_env_var(ctx->info->env, args[i]);
+	}
+	return (0);
 }
 
-int builtin_exit(char **args, t_exec_ctx *ctx)
+int	builtin_exit(char **args, t_exec_ctx *ctx)
 {
-    int exit_code = ctx->last_exit_status;
-    
-    printf("exit\n");
-    if (args[1] && args[2])
-        write_error("bash: exit: too many arguments\n");
-    if (args[1])
-    {
-        if (!is_valid_number(args[1]))
-        {
-            write_error_with_arg("bash: exit: ", args[1], ": numeric argument required\n");
-            exit_code = 2;
-        }
-        else
-        {
-            long val = ft_atoi(args[1]);
-            exit_code = ((val % 256) + 256) % 256;
-        }
-    }
-    ctx->info->exit_f = 0;
-    ctx->info->last_exit_status = exit_code;
-    return (exit_code);
+	int		exit_code;
+	long	val;
+
+	exit_code = ctx->last_exit_status;
+	printf("exit\n");
+	if (args[1] && args[2])
+		write_error("bash: exit: too many arguments\n");
+	if (args[1])
+	{
+		if (!is_valid_number(args[1]))
+		{
+			write_error_with_arg("bash: exit: ", args[1],
+				": numeric argument required\n");
+			exit_code = 2;
+		}
+		else
+		{
+			val = ft_atoi(args[1]);
+			exit_code = ((val % 256) + 256) % 256;
+		}
+	}
+	ctx->info->exit_f = 0;
+	ctx->info->last_exit_status = exit_code;
+	return (exit_code);
 }
